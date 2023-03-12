fea25d03fd5072bb99a9468f065b6156819f6eb9
Maxim Eryomenko <moeryomenko@gmail.com>
2023-03-12 21:39:25 +0300

    test: добавление базового класса для интеграционных тестов
    Signed-off-by: Maxim Eryomenko <moeryomenko@gmail.com>

---
+++ src/test/java/sa/ovodkov/librarian/AbstractIntegrationTest.java
@@ -0,0 +1,30 @@
+package sa.ovodkov.librarian;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public abstract class AbstractIntegrationTest {
+  @Container
+  public static final PostgreSQLContainer<?> postgres =
+      new PostgreSQLContainer<>("postgres:15")
+          .withDatabaseName("test-db")
+          .withUsername("sa")
+          .withPassword("pass")
+          .withReuse(true);
+
+  @DynamicPropertySource
+  static void configureProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.datasource.url", postgres::getJdbcUrl);
+    registry.add("spring.datasource.username", postgres::getUsername);
+    registry.add("spring.datasource.password", postgres::getPassword);
+    registry.add("spring.datasource.driver-class-name", postgres::getDriverClassName);
+  }
+}
