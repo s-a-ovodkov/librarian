bb036847d258ca455a489323e775c980ee23db0f
Sergey Ovodkov <ovodkov.s.a@gmail.com>
2023-04-16 14:44:39 +0300

    Переработаны интеграционные тесты

--- src/test/java/sa/ovodkov/librarian/AbstractIntegrationTest.java
+++ src/test/java/sa/ovodkov/librarian/AbstractIntegrationTest.java
@@ -1,30 +1,15 @@
 package sa.ovodkov.librarian;
 
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import com.github.database.rider.core.api.configuration.DBUnit;
+import com.github.database.rider.core.api.configuration.Orthography;
+import com.github.database.rider.junit5.api.DBRider;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.test.context.ActiveProfiles;
 
+@DBRider
+@DBUnit(caseInsensitiveStrategy = Orthography.LOWERCASE)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Testcontainers
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("it")
 public abstract class AbstractIntegrationTest {
-  @Container
-  public static final PostgreSQLContainer<?> postgres =
-      new PostgreSQLContainer<>("postgres:15")
-          .withDatabaseName("test-db")
-          .withUsername("sa")
-          .withPassword("pass")
-          .withReuse(true);
 
-  @DynamicPropertySource
-  static void configureProperties(DynamicPropertyRegistry registry) {
-    registry.add("spring.datasource.url", postgres::getJdbcUrl);
-    registry.add("spring.datasource.username", postgres::getUsername);
-    registry.add("spring.datasource.password", postgres::getPassword);
-    registry.add("spring.datasource.driver-class-name", postgres::getDriverClassName);
-  }
 }
