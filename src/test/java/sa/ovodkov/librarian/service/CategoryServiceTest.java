c7686a109afb7e145022106e19523657fa0c2f57
Sergey Ovodkov <ovodkov.s.a@gmail.com>
2023-05-09 18:06:18 +0300

    fix

--- src/test/java/sa/ovodkov/librarian/service/CategoryServiceTest.java
+++ src/test/java/sa/ovodkov/librarian/service/CategoryServiceTest.java
@@ -7,17 +7,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import sa.ovodkov.librarian.entity.Category;
 import sa.ovodkov.librarian.repository.CategoryRepository;
 
 import java.util.Optional;
-import java.util.Set;
 
 import static fixture.CategoryServiceTestData.getCategoryDTOWithParentCategory;
 import static fixture.CategoryServiceTestData.getCategoryDTOWithoutParentCategory;
 import static fixture.CategoryServiceTestData.getParentCategory;
-import static fixture.CategoryServiceTestData.getParentCategoryWithChildren;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.never;
@@ -54,33 +50,4 @@ class CategoryServiceTest {
 
         verify(categoryRepository, times(1)).findById(eq(PARENT_CATEGORY_ID));
     }
-
-//    @Test
-//    @DisplayName("Получение списка дочерних категорий, случай наличия подкатегорий")
-//    void getChildrenCategory() {
-//        Long[] children = {4L, 5L, 6L};
-//        when(categoryRepository.findById(anyLong())).thenReturn(Optional.of(getParentCategoryWithChildren(PARENT_CATEGORY_ID, children)));
-//
-//        Set<Category> result = categoryService.getSubCategories(PARENT_CATEGORY_ID);
-//
-//        verify(categoryRepository, times(1)).findById(eq(PARENT_CATEGORY_ID));
-//
-//        assertThat(result)
-//            .isNotNull()
-//            .hasSize(children.length)
-//            .extracting(Category::getId)
-//            .containsOnly(children);
-//    }
-//
-//    @Test
-//    @DisplayName("Получение списка дочерних категорий, случай отсутствия таковых у указанной категории")
-//    void test() {
-//        when(categoryRepository.findById(anyLong())).thenReturn(Optional.empty());
-//
-//        Set<Category> result = categoryService.getSubCategories(PARENT_CATEGORY_ID);
-//
-//        verify(categoryRepository, times(1)).findById(eq(PARENT_CATEGORY_ID));
-//
-//        assertThat(result).isNotNull().isEmpty();
-//    }
 }
